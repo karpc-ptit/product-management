91a66cc6c701d832fc0e2bbf264c5938d4346098
karpc-ptit <kpcuongz@gmail.com>
2025-09-05 07:48:12 +0700

    Nhung file tinh

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 //hết nhúng express
+
 // nhúng views (pug)
 app.set('views','./views');
 app.set('view engine','pug');
 // hết view
+
 //nhúng route
 const route = require("./routers/client/index.route")
 // hết nhúng route
+
+// nhúng file static
+app.use(express.static("public"));
+// hết nhúng file static
 route(app);
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
